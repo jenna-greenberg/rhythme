db29c5133c2ef6bcda5c8b9364754e5596c19ee8
Jenna-greenberg <jenna.greenberg@du.edu>
2025-09-03 22:57:35 -0500

    weekly-planner: firebase addition 09/03/25

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCd-dVuY8sb5u109P8snfGAeEVT4dP9lHw",
+  authDomain: "rhythme-1a438.firebaseapp.com",
+  projectId: "rhythme-1a438",
+  storageBucket: "rhythme-1a438.firebasestorage.app",
+  messagingSenderId: "1076994429203",
+  appId: "1:1076994429203:web:dc087fc7bd833baedacae6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
